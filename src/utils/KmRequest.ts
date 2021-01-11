e6bdbe1203f3eadeaea3d57212db2cd6fb9d8eb5
birnagerald <birnagerald.pro@gmail.com>
2021-01-11 18:24:26 +0100

    add: KM copyright

--- src/utils/KmRequest.ts
+++ src/utils/KmRequest.ts
@@ -1,3 +1,9 @@
+// © Contributeurs de la base de données Otaku de Karaoke Mugen 2017-2020 
+// - La base de données Otaku de Karaoke Mugen est rendue disponible sous les termes de la Open Database License. 
+// Tous les droits associés aux données présentes sont concédés sous la Database Contents License. 
+// Vous devriez avoir reçu une copie de ces licences avec la base de données. 
+// Le cas échéant, vous pouvez consulter https://opendatacommons.org/licenses/index.html
+
 import fetch, { Response } from "node-fetch";
 interface Data {
   infos: object;
